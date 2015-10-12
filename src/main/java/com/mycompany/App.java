5d732134cafd5be120ad86ad2bb07a7753a5b5a2
Mark Wait <marek.z.czekala@gmail.com>
2015-10-12 12:53:39 +0200

    Przygotowanie aplikacji do deployu na Tomcata

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -2,14 +2,26 @@ package com.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableAutoConfiguration
 @ComponentScan("com.mycompany")
-public class App {
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(applicationClass);
+	    }
+
+	    private static Class<App> applicationClass = App.class;
+	
 
 }
